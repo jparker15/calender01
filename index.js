103dd8f08e65bf88b2df81d0f2a91372b19bce7e
Jeremy Parker <jeremyeparker@yahoo.com>
2020-04-02 20:17:29 -0400

    added consoleLog on line 6

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 let currentDayOfWeek = "Monday",
     currentMonth = "April",
-    currentDay = 2;
+    currentDay = 2,
+    currentYear = 2020;
+
+    console.log(`Today is ${currentDayOfWeek},${currentMonth},${currentDay},${currentYear}`);
+    
